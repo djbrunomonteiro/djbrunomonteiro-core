69ff419b7fb64692da819410fae0626e25d4c2f8
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2023-09-29 13:20:08 -0300

    start projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'dev',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Module',
+      }).then((m) => m.AppModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
